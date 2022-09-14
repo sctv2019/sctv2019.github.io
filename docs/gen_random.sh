54023e19439924c2c8fc5f6c15a0f1f05d50c376
Your Name <you@example.com>
2022-09-14 09:58:40 -0400

    x

--- docs/gen_random.sh
+++ docs/gen_random.sh
@@ -8,7 +8,7 @@ func01(){
     <head>
         <script>
             var songList={};
-            console.log("b001 :", songList);
+            //console.log("b001 :", songList);
 
 EOF1
 }
@@ -21,50 +21,63 @@ func03(){
     cat >> ${bb1}.html << EOF3
 
             var len=Object.keys(songList).length;
-            console.log("b003 : songList length : ", len);
+            //console.log("b003 : songList length : ", len);
+
+            var index=0 ;
+            var result="" ;
+            var title2=" 暫停，等待用戶按鍵 " ;
 
             function randomChoice(choices) {
                             var rand = Math.random() ;
-                            console.log("b101 : rand : ", rand);
+                            //console.log("b101 : rand : ", rand);
 
-                            var index = Math.floor( rand * len);
-                            console.log("b103 : index : ", index);
+                            index = Math.floor( rand * len);
+                            //console.log("b103 : index : ", index);
 
-                            var result = choices['a_' + index];
-                            console.log("b109 : result : ", result);
+                            result = choices['a_' + index];
+                            //console.log("b109 : result : ", result);
 
                             return result ;
                         }
 
             function playAudio() {
                             try{
-                                            console.log("b201 :", "try_pauseing then start next 1 ");
+                                            //console.log("b201 :", "try_pauseing then start next 1 ");
                                             x.pause();
-                                            console.log("b201 :", "try_pauseing then start next 2 ");
+                                            //console.log("b201 :", "try_pauseing then start next 2 ");
                                         }
                             catch(err){
                                         }
                             finally{
                                             t = randomChoice(songList);
+
                                             const pp1 = document.getElementById("p1");
                                             pp1.innerHTML = t ;
-                                            console.log("b401 : try play", t );
+                                            //console.log("b401 : try play", t );
 
                                             let ff1 = t.lastIndexOf("/") + 1;
                                             let sub1 = t.substr(ff1);
                                             let ff2 = sub1.indexOf(".") ;
                                             let sub2 = sub1.substr(0,ff2);
-                                            const pp2 = document.getElementById("p2");
-                                            pp2.innerHTML = sub2 ;
+                                            title2 = sub2 ;
 
-                                            console.log("b401 : try play", t );
+                                            const pp2 = document.getElementById("p2");
+                                            pp2.innerHTML = "播放中 : " + index + " : " + title2 ;
+
+                                            const pp3 = document.getElementById("p3");
+                                            pp3.innerHTML = " == ${bb2} / " + len + " ==" ;
+
+
+                                            //console.log("b401 : try play", t );
                                             x = new Audio(t);
                                             x.play();
                                         }
                         };
             function pauseAudio() {
-                            console.log("b801 :", "stop only ");
+                            //console.log("b801 :", "stop only ");
                             x.pause();
+                            const pp3 = document.getElementById("p2");
+                            pp3.innerHTML = "暫停 : " + index + " : " + title2 ;
                         }
         </script>
 
@@ -72,8 +85,9 @@ func03(){
     <body>
         <button style="font-size: 150px; background-color: #4CAF50; border-radius: 25%; " onclick="playAudio()" > 播放 </button></h1>
     <button style="font-size: 150px; background-color: #f44336; border-radius: 25%; " onclick="pauseAudio()"> 暫停 </button>
-    <p id="p1">Hello World!</p>
-    <h1 style="font-size:3vw" id="p2" > Hello World2 </h1>
+    <p id="p1">暫停，等待用戶按鍵 !</p>
+    <h1 style="font-size:3vw" id="p2" > 暫停，等待用戶按鍵  !</h1>
+    <h1 style="font-size:5vw" id="p3" > === ${bb2} === </h1>
     </body>
 </html
 
@@ -94,6 +108,7 @@ func021(){
 }
 
 bb1=r_laohei
+bb2='公民老黑'
 func01 
 for aa1 in laohei2019.mp3.rss laohei2020.mp3.rss laohei2021.mp3.rss laohei2022.mp3.rss
 do
@@ -103,14 +118,20 @@ func03
 
 
 bb1=r_sctv
+bb2='麻辣空間'
+func01 
 for aa1 in sctv2019.mp3.rss sctv2020.mp3.rss sctv2021.mp3.rss sctv2022.mp3.rss
 do
     func021 ${aa1}
 done
+func03
 
 bb1=r_yolo
+bb2='yolo街'
+func01 
 for aa1 in yolo2022.mp3.rss
 do
     func021 ${aa1}
 done
+func03
 
